8f96e06ac082fbcc1a127bc30871e7699b637e58
boss11ankit <jhaankit0110@gmail.com>
2024-08-25 18:45:41 +0530

    FIRST COMMIT

---
+++ src/primeCheck.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class primeCheck {
+    public static void main(String[]args){
+        System.out.print("enter the number :");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        boolean isPrime = true;
+        for(int i = 2; i <= num/2; i++){
+            if (num%i==0){
+               isPrime = false;
+               break;
+            }
+
+        }
+        if(isPrime){
+            System.out.print( num +  "is prime no");
+        }
+        else{
+            System.out.print(num +  "is not prime");
+        }
+    }
+
+}
