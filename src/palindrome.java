fd3766b20ad9e7243d5095274e7913c65bb3556b
boss11ankit <jhaankit0110@gmail.com>
2024-08-25 18:24:52 +0530

    FIRST COMMIT

--- src/palindrome.java
+++ src/palindrome.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
-public class reverseString{
+public class palindrome {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
         System.out.print("enter your string: ");
         String str = sc.nextLine();
         String reversed = new StringBuilder(str).reverse().toString();
-        System.out.println("reversed string: " +reversed);
-
+        if (str.equals(reversed)){
+            System.out.print(str + " is palindrome");
+        }
+        else{
+            System.out.print(str + " is not palindrome");
+        }
     }
 }
