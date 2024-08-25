046b640e61d524b04b7d70b76291d30b5d710afe
boss11ankit <jhaankit0110@gmail.com>
2024-08-25 21:42:42 +0530

    FIRST COMMIT

--- src/largestofThreenum.java
+++ src/largestofThreenum.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 public class largestofThreenum {
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        System.out.print("eenter first number: ");
-        int num1 = sc.nextInt();
-        System.out.print("enter second number: ");
-        int num2 = sc.nextInt();
-        System.out.print("enter third number: ");
-        int num3 = sc.nextInt();
-        int largest = Math.max(num1,Math.max(num2,num3));
-        System.out.println("largest no among three is : " + largest);
+       int i = sc.nextInt();
+       double d = sc.nextDouble();
+       sc.nextLine();
+       String str = sc.nextLine();
+       System.out.println(i);
+       System.out.println(d);
+       System.out.println(str);
+       sc.close();
+
+
     }
 }
