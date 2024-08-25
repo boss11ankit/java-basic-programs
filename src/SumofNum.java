6725451cc2b6ad57501f8b1597930b8044a7d9f8
boss11ankit <jhaankit0110@gmail.com>
2024-08-25 22:15:43 +0530

    FIRST COMMIT

--- src/SumofNum.java
+++ src/SumofNum.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 public class SumofNum {
-      public static void main(String[]args){
-          Scanner sc = new Scanner(System.in);
-          System.out.print("enter the number");
-          int num = sc.nextInt();
-          int sum = 0;
-          while (num!=0){
-              sum+=num%10;
-              num/=10;
-          }
-          System.out.println(sum);
-      }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter the number");
+        int num = sc.nextInt();
+        int sum = 0;
+        while (num != 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        System.out.println(sum);
+    }
 }
