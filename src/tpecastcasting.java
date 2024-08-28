75b5443fb10fc9eeb66c6877a0b1400649fee507
boss11ankit <jhaankit0110@gmail.com>
2024-08-28 23:59:21 +0530

    FIRST COMMIT

---
+++ src/tpecastcasting.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class tpecastcasting {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int a = 10;
+        byte b =(byte)a;
+        System.out.print(a);
+
+
+    }
+}
